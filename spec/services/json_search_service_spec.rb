db0ad8f9c1fe7310c44d92dc0d43e170453cf6f2
Rustam Gasanov <rustamagasanov@gmail.com>
2015-03-29 17:33:45 +0300

    [-] removed unused spec

--- spec/services/json_search_service_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SearchService do
-  # it { expect(described_class).to respond_to(:load) }
-  # it { expect{ described_class.load }.to_not raise_exception }
-  # it { expect(described_class.load).to be_kind_of(Array) }
-end
